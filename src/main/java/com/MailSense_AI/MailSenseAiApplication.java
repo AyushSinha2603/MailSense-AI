162d6ac1f2bda8fb2937cac263bdd47f70d41b9b
AyushSinha2603 <ayushcodes26@gmail.com>
2025-08-23 08:15:57 +0530

    api integration

---
+++ src/main/java/com/MailSense_AI/MailSenseAiApplication.java
@@ -0,0 +1,13 @@
+package com.MailSense_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenseAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MailSense_AI/MailSenseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MailSense_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
